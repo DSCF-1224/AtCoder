ed900945178829f734873a557345aba195916e37
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2019-07-16 06:51:55 +0900

    2019.07.16 AM06:00
    [1/3] clear ABC001 task A by Haskell\ABC\ABC001\ABC001_A_v01.hs
    [2/3] clear ABC001 task A by Haskell\ABC\ABC001\ABC001_A_v02.hs
    [3/3] update Haskell\README.md

---
+++ Haskell/ABC/ABC001/ABC001_A_v02.hs
@@ -0,0 +1,37 @@
+-- [contest]  ABC 001
+-- [task]     A
+-- [URL]      https://atcoder.jp/contests/abc001/tasks/abc001_1
+-- [compiler] Haskell (GHC 7.10.3)
+-- [status]   https://atcoder.jp/contests/abc001/submissions/6401389 ; AC
+
+-- [reference]
+-- http://hackage.haskell.org/package/bytestring-0.10.8.2/docs/Data-ByteString-Char8.html
+-- https://qiita.com/hnw/items/3f7d27b742c5a1a99a9a
+-- https://myuon.github.io/posts/haskell-atcoder/
+
+-- modules to import --
+
+import qualified Data.ByteString.Char8
+import qualified Data.Maybe
+import           Prelude
+
+-- functions for this task --
+
+read_Integer = Prelude.fst . Data.Maybe.fromJust . Data.ByteString.Char8.readInteger
+get_Integer  = read_Integer <$> Data.ByteString.Char8.getLine
+
+-- the main process is as follows --
+
+main :: IO ()
+main = do
+
+        -- STEP.01
+        -- read out the given data
+        height_1st <- get_Integer
+        height_2nd <- get_Integer
+
+        -- STEP.02
+        -- calculate & output the answer of this task
+        Prelude.print $ Prelude.uncurry (-) (height_1st, height_2nd)
+
+-- EOF --
