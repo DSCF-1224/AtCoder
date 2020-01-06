ec3e9bb739957fef92dc4b1a82b703540a341f21
DSCF_1224 <37315588+DSCF-1224@users.noreply.github.com>
2020-01-06 23:36:32 +0900

    2020.01.06 PM11
    [1/1] update solutions of CPP14(GCC5.4.1)
    1. ABC 002 D v01

--- CPP14(GCC 5.4.1)/ABC/ABC002/ABC002_D_v01.cpp
+++ CPP14(GCC 5.4.1)/ABC/ABC002/ABC002_D_v01.cpp
@@ -4,6 +4,9 @@
 // compiler : C++14 (GCC 5.4.1)
 // status   : Not Submitted
 
+// - reference
+//   - https://abc002.contest.atcoder.jp/submissions/110897
+
 #include <algorithm>
 #include <iostream>
 #include <string>
@@ -15,7 +18,7 @@ namespace abc002
 
 	namespace task_D
 	{
-		struct Connection
+		struct PairConnection
 		{
 			/* Data Member */
 			public:
@@ -25,7 +28,7 @@ namespace abc002
 			/* Constructor */
 			public:
 				explicit
-					Connection (int const & fst, int const & snd)
+					PairConnection (int const & fst, int const & snd)
 					{
 						this->fst = fst;
 						this->snd = snd;
@@ -33,7 +36,7 @@ namespace abc002
 
 			public:
 				explicit
-					Connection (int const & val)
+					PairConnection (int const & val)
 					{
 						this->fst = val;
 						this->snd = val;
@@ -41,8 +44,8 @@ namespace abc002
 
 			public:
 				explicit
-					Connection (void):
-						Connection(0)
+					PairConnection (void):
+						PairConnection(0)
 					{/* nothing to do in this block scope */}
 
 			/* Member function */
@@ -54,19 +57,67 @@ namespace abc002
 						>> this->snd
 					;
 				}
+
+			public:
+				void cout_member (void) const
+				{
+					std::cout
+						<< "("s
+						<< this->fst
+						<< ","s
+						<< this->snd
+						<< ")\n"s
+					;
+				}
 		};
 
 		struct Num
 		{
 			/* Data Member */
 			public:
-				int representative;
-				int connection;
+				std::size_t representative;
+				std::size_t connection;
+
+			/* Member function */
+			private:
+				bool inspector_representative (void) const
+				{
+					return this->representative > 0;
+				}
+
+			private:
+				bool inspector_connection (void) const
+				{
+					return this->connection > 0;
+				}
+
+			public:
+				void inspector (void) const
+				{
+					try
+					{
+						if ( ! this->inspector_representative() )
+						{
+							throw std::out_of_range("The number of representatives was not positive integer !\n"s);
+						}
+						else if ( ! this->inspector_connection() )
+						{
+							throw std::out_of_range("The number of connections was not positive integer !\n"s);
+						}
+					}
+					catch (const std::exception & e)
+					{
+						std::cerr << e.what();
+						std::abort();
+					}
+
+					return;
+				}
 
 			/* Constructor */
 			public:
 				explicit
-					Num (int const & representative, int const & connection)
+					Num (std::size_t const & representative, std::size_t const & connection)
 					{
 						this->representative = representative;
 						this->connection     = connection;
@@ -74,16 +125,8 @@ namespace abc002
 
 			public:
 				explicit
-					Num (int const & val)
-					{
-						this->representative = val;
-						this->connection     = val;
-					}
-
-			public:
-				explicit
 					Num (void):
-						Num(0)
+						Num(1, 0)
 					{/* nothing to do in this block scope */}
 
 			/* Member function */
@@ -94,35 +137,86 @@ namespace abc002
 						>> this->representative
 						>> this->connection
 					;
+
+					this->inspector();
+					
+					return;
+				}
+
+			public:
+				void cout_member (void) const
+				{
+					std::cout
+						<< "representative : "s << this->representative << "\n"s
+						<< "relation       : "s << this->connection     << "\n"s
+					;
 				}
 		};
 
 		struct Data:
 			public Num,
-			public Connection
+			public PairConnection
 		{
 			/* Data Member */
 			private:
-				Num                     num;
-				std::vector<Connection> connection;
+				Num                            num;
+				std::vector<PairConnection>    pair_connection;
+				std::vector<std::vector<bool>> stat_connection;
+
+			/* Member Function */
+			private:
+				void resize_StatConnection (void)
+				{
+					this->stat_connection.resize( this->num.representative );
+
+					for (auto iter = std::begin( this->stat_connection ), iter_end = std::end( this->stat_connection ); iter != iter_end; ++iter)
+					{
+						(*iter).resize( this->num.representative );
+					}
+
+					return;
+				}
+
+			private:
+				void resize_PairConnection (void)
+				{
+					this->pair_connection.resize( this->num.connection );
+				}
 
 			/* Constructor */
 			public:
 				explicit
-					Data (Num const & obj_num, std::vector<Connection> const & obj_connection)
-					{
-						// STEP.01
+					Data
+					(
+						Num                            obj_num,
+						std::vector<PairConnection>    obj_pair_connection,
+						std::vector<std::vector<bool>> obj_stat_connection
+					) {
+						// STEP.01.01
+						obj_num.inspector();
+
+						// STEP.01.02
 						this->num = obj_num;
 
 						// STEP.02.01
-						this->connection.resize( std::size(  obj_connection ) );
+						this->resize_PairConnection();
 
 						// STEP.02.02
 						std::copy(
-							std::begin ( obj_connection ),
-							std::end   ( obj_connection ),
-							std::begin ( this->connection )
+							std::begin ( obj_pair_connection   ),
+							std::end   ( obj_pair_connection   ),
+							std::begin ( this->pair_connection )
 						);
+
+						// STEP.03.01
+						this->resize_StatConnection();
+
+						// STEP.03.02
+						for (std::size_t iter1 = 0; iter1 < obj_num.representative; ++iter1)
+						for (std::size_t iter2 = 0; iter2 < obj_num.representative; ++iter2)
+						{
+							this->stat_connection.at(iter1).at(iter2) = obj_stat_connection.at(iter1).at(iter2);
+						}
 					}
 
 			public:
@@ -133,6 +227,7 @@ namespace abc002
 					}
 
 			/* Member Function */
+
 			public:
 				void cin (void)
 				{
@@ -140,12 +235,23 @@ namespace abc002
 					this->num.cin();
 
 					// STEP.02.01
-					this->connection.resize( this->num.connection );
+					this->resize_PairConnection();
 
 					// STEP.02.02
-					for ( auto iter = std::begin( this->connection ), iter_end = std::end( this->connection ); iter != iter_end; ++iter )
+					std::for_each (
+						std::begin( this->pair_connection ),
+						std::end  ( this->pair_connection ),
+						[] (auto & iter) { iter.cin(); }
+					);
+
+					// STEP.03.01
+					this->resize_StatConnection();
+
+					// STEP.03.02
+					for (std::size_t iter = 0; iter < this->num.connection; ++iter)
 					{
-						(*iter).cin();
+						this->stat_connection.at( this->pair_connection.at(iter).fst - 1 ).at( this->pair_connection.at(iter).snd - 1 ) =
+						this->stat_connection.at( this->pair_connection.at(iter).snd - 1 ).at( this->pair_connection.at(iter).fst - 1 ) = true;
 					}
 
 					// STEP.TRUE_END
@@ -153,26 +259,46 @@ namespace abc002
 				}
 
 			public:
-				void cout_member (void)
+				void cout_member (void) const
 				{
 					// STEP.01
-					std::cout
-						<< "representative : "s << this->num.representative << "\n"s
-						<< "relation       : "s << this->num.connection     << "\n"s
-					;
+					this->num.cout_member();
 
 					// STEP.02
-					for ( auto iter = std::begin( this->connection ), iter_end = std::end( this->connection ); iter != iter_end; ++iter )
+					std::for_each (
+						std::begin( this->pair_connection ),
+						std::end  ( this->pair_connection ),
+						[] (auto & iter) { iter.cout_member(); }
+					);
+
+					// STEP.03
+					std::cout << std::noboolalpha;
+
+					for (std::size_t iter = 0; iter < this->num.representative; ++iter)
 					{
-						std::cout
-							<< "("s
-							<< (*iter).fst
-							<< ","s
-							<< (*iter).snd
-							<< ")\n"s
-						;
-					}
+						std::for_each (
+							std::begin( this->stat_connection.at(iter) ),
+							std::end  ( this->stat_connection.at(iter) ),
+							[](auto obj_iter)
+							{
+								std::cout << obj_iter;
+							}
+						);
+
+						std::cout << "\n"s;
+					};
 				}
+
+			public:
+				int MaxFactionalRepresentatives (void) const
+				{
+					/* return value of this function */
+					int max_Representative = 0;
+
+					// STEP.01
+					return max_Representative;
+				}
+
 		};
 		
 	} // namespace task_D
